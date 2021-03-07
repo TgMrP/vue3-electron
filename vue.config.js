d000523cac9fdfb853be54db21acf017d6bb0b9c
Ran Tayar <tgmrp.dev@gmail.com>
2021-03-07 14:25:39 +0200

    affffffffffffff

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,6 @@ module.exports = {
     electronBuilder: {
       builderOptions: {
         publish: ['github'],
-        'snap.publish': {
-          provider: 'generic',
-          url: 'https://anydummyurl.com',
-        },
       },
     },
   },
